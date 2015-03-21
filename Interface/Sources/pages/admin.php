91c69a16da5173c53b8e17a00a6275cf4e4ecf8d
Lucas Franceschino <lucas@franceschino.fr>
2015-03-21 22:59:29 +0100

    Admin peut éditer Présentation

--- Interface/Sources/pages/admin.php
+++ Interface/Sources/pages/admin.php
@@ -36,25 +36,62 @@
 	.panel{
 		float: left;
 		width: 760px;
-		height: 500px;
 	}
 </style>
-<div ng-controller="admin" ng-init="currentPage = 0; pages = [&quot;Encadrants&quot;, &quot;Groupes&quot;, &quot;Présentation&quot;, &quot;Modalité notes&quot;];" class="page interface-admin">
-	<h1 ng-repeat="(k,page) in pages" state="{{currentPage==k}}" ng-click="$parent.currentPage=k">{{page}}</h1><br/>
-	<div class="view">
-		<div state="{{currentPage}}" class="panels">
-			<div class="panel">
-				<h2>[Page à inclure ici] ~T1</h2>
-			</div>
-			<div class="panel"> 
-				<h2>[Page à inclure ici] ~T2</h2>
-			</div>
-			<div class="panel"> 
-				<h2>[Page à inclure ici] ~T3</h2>
-			</div>
-			<div class="panel"> 
-				<h2>[Page à inclure ici] ~T4</h2>
-			</div>
+<script type="text/javascript" src="TinyEditor/packed.js"></script>
+<link rel="stylesheet" href="TinyEditor/style.css"/><?php if(isset($parseParam[1], $parseParam[2], $_POST['content']) && $parseParam[1]=='presentation' && $parseParam[2]=='update'){
+	$html = explode('<style>*{font-family: \'Open Sans\';}</style>', $_POST['content'], 2)[1];
+	file_put_contents('pages/presentation-content.html', $html);
+}
+echo '<div class=\'page interface-admin\' ng-controller=\'admin\' ng-init=\'';
+echo 'pages = ["Encadrants", "Groupes", "Présentation", "Modalité notes"]; currentPage = 0;';
+if(isset($parseParam[1]) && $parseParam[1]=='presentation')
+	echo 'currentPage = 2;';
+echo '\'>';
+ ?>
+<h1 ng-repeat="(k,page) in pages" state="{{currentPage==k}}" ng-click="$parent.currentPage=k">{{page}}</h1><br/>
+<div class="view">
+	<div state="{{currentPage}}" class="panels">
+		<div class="panel">
+			<h2>[Page à inclure ici] ~T1</h2>
+		</div>
+		<div class="panel"> 
+			<h2>[Page à inclure ici] ~T2</h2>
+		</div>
+		<div class="panel"> 
+			<h2>Page de présentation</h2>
+			<form method="POST" action="/?admin/presentation/update">
+				<textarea id="input" name="content" style="width:760px; height:400px; margin: auto;">
+					<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300"/>
+					<style>*{font-family: 'Open Sans';}</style><?php echo file_get_contents('pages/presentation-content.html'); ?>
+				</textarea>
+				<script>
+					var editor = new TINY.editor.edit('editor',{
+						id:'input',
+						width:660,
+						height:300,
+						cssclass:'te',
+						controlclass:'tecontrol',
+						rowclass:'teheader',
+						dividerclass:'tedivider',
+						controls:['bold','italic','underline','strikethrough','|','subscript','superscript','|',
+								  'orderedlist','unorderedlist','|','outdent','indent','|','leftalign',
+								  'centeralign','rightalign','blockjustify','|','unformat','|','undo','redo','n',
+								  'image','hr','link','unlink','|','cut','copy','paste','print'],
+						footer:true,
+						fonts:['Open Sans','Arial','Georgia','Trebuchet MS'],
+						xhtml:true,
+						cssfile:'style.css',
+						bodyid:'editor',
+						footerclass:'tefooter',
+						resize:{cssclass:'resize'}
+					});
+				</script>
+				<button onclick="editor.post();">Mettre à jour</button>
+			</form>
+		</div>
+		<div class="panel"> 
+			<h2>[Page à inclure ici] ~T4</h2>
 		</div>
 	</div>
-</div>
+</div><?php echo '</div>'; ?>
--- Interface/Sources/pages/presentation.php
+++ Interface/Sources/pages/presentation.php
@@ -1,9 +1,6 @@
 
 <div class="page presentation">
 	<h1>Présentation</h1><img src="images/logo_fds.png" style="float: left;" class="fds"/>
-	<div class="text">
-		<p>Lorem ipsum In consequat enim culpa irure adipisicing commodo enim deserunt dolore dolor ex minim in reprehenderit ut sit reprehenderit qui ea elit Excepteur adipisicing Duis consectetur amet ut dolor consequat.</p>
-		<p>Lorem ipsum In consequat enim culpa irure adipisicing commodo enim deserunt dolore dolor ex minim in reprehenderit ut sit reprehenderit qui ea elit Excepteur adipisicing Duis consectetur amet ut dolor consequat.</p>
-		<p>Lorem ipsum In consequat enim culpa irure adipisicing commodo enim deserunt dolore dolor ex minim in reprehenderit ut sit reprehenderit qui ea elit Excepteur adipisicing Duis consectetur amet ut dolor consequat.</p>
+	<div class="text"><?php echo file_get_contents('pages/presentation-content.html'); ?>
 	</div>
 </div>
