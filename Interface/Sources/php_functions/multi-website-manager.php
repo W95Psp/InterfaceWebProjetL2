1be7852581c820307d251359b9e69d2c548bc78c
Lucas Franceschino <lucas@franceschino.fr>
2015-03-27 22:49:52 +0100

    Fix redirection

--- Interface/Sources/php_functions/multi-website-manager.php
+++ Interface/Sources/php_functions/multi-website-manager.php
@@ -1,15 +1,12 @@
 <?php
 	$versionWebsite = 'none';
-	$versionWebsiteUrl = 'none';
 	@array_walk(json_decode(file_get_contents('config/versions-website.json')),function($version, $url){
 		global $versionWebsite;
-		if(substr($_SERVER['HTTP_HOST'], 0, strlen($url))==$url){
-			$versionWebsiteUrl = $url;
+		if(substr($_SERVER['HTTP_HOST'], 0, strlen($url))==$url)
 			$versionWebsite = $version;
-		}
 	});
 
-	if(preg_match_all("/^<redirect-to\:(.*)>$/", $versionWebsiteUrl, $r)){
+	if(preg_match_all("/^<redirect-to\:(.*)>$/", $versionWebsite, $r)){
 		die('Erreur, la ressource à été déplacée. <a href="'.$r[1][0].'">Cliquez ici pour être redirigé</a>');
 	}
 
