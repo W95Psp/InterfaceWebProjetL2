8588e35122c1bdcffee9e58b91a4e011a362ebea
Lucas Franceschino <lucas@franceschino.fr>
2015-04-03 17:28:07 +0200

    ConfirmModule

--- Interface/Sources/php_functions/ask_module.php
+++ Interface/Sources/php_functions/ask_module.php
@@ -1,123 +1,132 @@
 <?php
-
-function execThisAction($action){
-	if($action['type']=='delete'){
-		// if($action['subject']=='')
+class ConfirmModule{
+	private static function exec($action){
+		if($action['action']=='delete'){
+			// if($action['subject']=='')
+		}
 	}
-}
 
-function askPeoples($listOfStudentsId, $listOfTeachersId, $objectJson){
-	global $db;
-	$sql = '
-SELECT  `idEtu` as id, `nomEtu` as nom, TRUE as isEleve, `prenomEtu` as prenom,
-		`emailEtu` as email FROM `Etudiant`
-	WHERE idEtu IN ('.implode(',', array_map('intval', $listOfStudentsId)).')
+	private static function sendEmail($profile, $actionId, $subject){
 
-UNION ALL
-
-SELECT  `idEns` as id, `nomEns` as nom, FALSE as isEleve, `prenomEns` as prenom,
-		`emailEns` as email FROM `Enseignent`
-	WHERE idEns IN ('.implode(',', array_map('intval', $listOfTeachersId)).');';
-
-	$listPeople = array();
-	foreach ($listOfStudentsId as $id)
-		$listPeople[] = intval($id);
-	foreach ($listOfTeachersId as $id)
-		$listPeople[] = 'p'.intval($id);
-
-	echo json_encode($listPeople);
-
-	$listPeople = implode(',', $listPeople);
-
-
-	$res = $db->query('INSERT INTO Action_sous_confirmation (concernedPeople, dataJson, expiration, idPersonInCharge, isPersonInChargeAStudent) VALUES ("'.$listPeople.'", "'.$db->real_escape_string(json_encode($objectJson)).'", DATE_ADD( utc_timestamp( ) , INTERVAL 4 DAY), '.intval(getUserId()).', '.intval(getUserType()==ELEVE).')') or die(mysqli_error($db));
-	
-	$res = $db->query($sql) or die(mysqli_error($db));
-
-	while (NULL !== ($someone = $res->fetch_array())){
-		echo '</hr>';
-		echo 'Envoyer un mail à : '.$someone['prenom'].' '.$someone['nom'];
-		echo '<br/>';
-		echo '<br/>';
-		$subject = 'Something';
-		$message = '<h1></h1>';
 	}
-}
 
-function getNoticesForMe(){
-	global $db;
-	$notices = array();
-	if(getUserType()==ANONYME)
-		return $notices;
-	$res = $db->query('SELECT * FROM Action_sous_confirmation WHERE isPersonInChargeAStudent='.intval(getUserType()==ELEVE).' AND idPersonInCharge='.intval(getUserId())) or die(mysqli_error($db));
+	public static function registerAction($listOfStudentsId, $listOfTeachersId, $objectJson){
+		global $db;
 
-	while (NULL !== ($row = $res->fetch_array())){
-		$row['dataJson'] = json_decode($row['dataJson'], true);
-		$notices[] = $row;
+		// SQL Querty to select emails of every peoples involved
+		$sql = '
+	SELECT  `idEtu` as id, `nomEtu` as nom, TRUE as isEleve, `prenomEtu` as prenom,
+			`emailEtu` as email FROM `Etudiant`
+		WHERE idEtu IN ('.implode(',', array_map('intval', $listOfStudentsId)).')
+
+	UNION ALL
+
+	SELECT  `idEns` as id, `nomEns` as nom, FALSE as isEleve, `prenomEns` as prenom,
+			`emailEns` as email FROM `Enseignent`
+		WHERE idEns IN ('.implode(',', array_map('intval', $listOfTeachersId)).');';
+
+		//Let's build the list (teacher prefixed by "p")
+		$listPeople = array();
+		foreach ($listOfStudentsId as $id)
+			$listPeople[] = intval($id);
+		foreach ($listOfTeachersId as $id)
+			$listPeople[] = 'p'.intval($id);
+
+		$listPeople = implode(',', $listPeople);
+
+		$res = $db->query('INSERT INTO Action_sous_confirmation (concernedPeople, dataJson, expiration, idPersonInCharge, isPersonInChargeAStudent) VALUES ("'.$listPeople.'", "'.$db->real_escape_string(json_encode($objectJson)).'", DATE_ADD( utc_timestamp( ) , INTERVAL 4 DAY), '.intval(getUserId()).', '.intval(getUserType()==ELEVE).')') or die(mysqli_error($db));
+		
+		$res = $db->query($sql) or die(mysqli_error($db));
+
+		while (NULL !== ($someone = $res->fetch_array()))
+			self::sendEmail($someone, $db->insert_id, @json_decode($objectJson, true)['explain']);
 	}
-	return $notices;
-}
 
-function confirmOrDeclineLink($idToConfirm, $confirmOrNot = true){
-	global $db;
-	$type = getUserType();
-	if($type>ANONYME){
-		$id = (($type==ELEVE)?'':'p').intval(getUserId());
-		$res = $db->query('SELECT dataJson, concernedPeople FROM Action_sous_confirmation WHERE id='.intval($idToConfirm).' AND FIND_IN_SET("'.$id.'", concernedPeople)') or die(mysqli_error($db));
-		$rows = array();
-		while (NULL !== ($row = $res->fetch_array()))
-			$rows[] = array(
+	public static function getUserByArray($arr){
+		$id = intval($arr['value']);
+		global $db;
+		if($arr['isStudent'])
+			$r=$db->query('SELECT nomEtu as nom, prenomEtu as prenom FROM Etudiant WHERE idEtu='.$id)->fetch_array();
+		else
+			$r=$db->query('SELECT nomEns as nom, prenomEns as prenom FROM Enseignent WHERE idEns='.$id)->fetch_array();
+		return $r["prenom"].' '.$r["nom"];
+	}
+
+	public static function getNotices(){
+		global $db;
+		$notices = array();
+		$id = intval(getUserId());
+		$isStudent = intval(getUserType()==ELEVE);
+		//We take only 
+		$res = $db->query('SELECT `id`, `idPersonInCharge`, `isPersonInChargeAStudent`, `concernedPeople`, `dataJson`, (concernedPeople LIKE "%-%") as canceled, (expiration < NOW() && expiration!=0) as expired FROM `Action_sous_confirmation` WHERE `idPersonInCharge`='.$id.' AND `isPersonInChargeAStudent`='.$isStudent) or die(mysqli_error($db));
+
+
+		while (NULL !== ($row = $res->fetch_array())){
+			$row['dataJson'] = json_decode($row['dataJson'], true);
+			$row['concernedPeople'] = self::countAndOrder($row['concernedPeople']);
+			$notices[] = array(
+				'id' => $row['id'],
+				'idPersonInCharge' => $row['idPersonInCharge'],
+				'isPersonInChargeAStudent' => $row['isPersonInChargeAStudent'],
+				'concernedPeople' => $row['concernedPeople'],
 				'dataJson' => $row['dataJson'],
-				'concernedPeople' => explode(',', $row['concernedPeople'])
-				);
-		if(count($rows)==0)
-			return false;
-		$row = $rows[0];
-		$concernedPeople = '';
-		$numberOfAgree = 0;
-		$numberOfDisagree = 0;
-		$numberOfNothing = 0;
-		while($concernedSomeone = array_pop($row['concernedPeople'])){
-			if($concernedPeople!='')
-				$concernedPeople .= ',';
-			if($concernedSomeone.''==$id.'')
-				$concernedSomeone = ($confirmOrNot?'+':'-').$concernedSomeone;
-			$numberOfAgree   +=($concernedSomeone[0]=='+');
-			$numberOfDisagree+=($concernedSomeone[0]=='-');
-			$numberOfNothing +=($concernedSomeone[0]!='-' && $concernedSomeone[0]!='+');
-			$concernedPeople .= $concernedSomeone;
+				'canceled' => $row['canceled'],
+				'expired' => $row['expired']
+			);
 		}
-		if($numberOfNothing==0 && $numberOfDisagree==0){//Tout le monde il est content
-			$db->query('DELETE FROM Action_sous_confirmation WHERE id='.intval($idToConfirm)) or die(mysqli_error($db));
-			execThisAction(json_decode($row['dataJson']));
-		}else{
-			if(!$confirmOrNot){
-
-			}
-			//On mets à jour, quand même !
-			$db->query('UPDATE Action_sous_confirmation SET concernedPeople="'.$db->real_escape_string($concernedPeople).'" WHERE id='.intval($idToConfirm)) or die(mysqli_error($db));
-		}
-		return true;
+		return $notices;
 	}
-}
 
-//To delete
-// define("ANONYME", 0);
-// define("ELEVE", 1);
-// define("ENCADRANT", 2);
-// define("ADMIN", 3);
-// $fakeAccount = array(
-// 	'type'=> ENCADRANT,
-// 	'id'=>	1002
-// );
-// function getUserType(){
-// 	global $fakeAccount;
-// 	return $fakeAccount['type'];
-// }function getUserId(){
-// 	global $fakeAccount;
-// 	return $fakeAccount['id'];
-// }
-//Stop to delete
+	private static function countAndOrder($concernedPeople){
+		$ids = array('list+'=>array(), 'list-'=>array(), 'list'=>array(), 'all'=>array());
+		foreach (explode(',', $concernedPeople) as $current){
+			if(!$current)continue;
+			$prefix = ($current[0]=='+'||$current[0]=='-')?$current[0]:'';
+			$value = ($current[0]=='+'||$current[0]=='-')?substr($current, 1):$current;
+			if(!$prefix && $current.''==getUserId().'')
+				$prefix = '+';
+			$ids['list'.$prefix][] = array(
+				'isStudent' => $value[0]!='p',
+				'value' => ($value[0]=='p')?substr($value, 1):$value
+			);
+			$ids['all'][] = $prefix.$value;
+		}
+		$ids['list?'] = $ids['list'];
+		return $ids;
+	}
+
+	public static function delete($idToConfirm){
+		global $db;
+		$db->query('DELETE FROM Action_sous_confirmation WHERE id='.intval($idToConfirm).' AND idPersonInCharge='.intval(getUserId()).' AND isPersonInChargeAStudent='.intval(getUserType()==ELEVE));
+	}
+
+	public static function forceAction($idToConfirm){
+		global $db;
+		$res = $db->query('SELECT * FROM Action_sous_confirmation WHERE id='.intval($idToConfirm).' AND idPersonInCharge='.intval(getUserId()).' AND isPersonInChargeAStudent='.intval(getUserType()==ELEVE))->fetch_array();
+		self::exec(json_decode($res['dataJson'], true));
+		self::delete($idToConfirm);
+	}
+	
+	public static function confirmOrDecline($idToConfirm, $confirmOrNot = true){
+		global $db;
+		$type = getUserType();
+		if($type>ANONYME){
+			$id = (($type==ELEVE)?'':'p').intval(getUserId());
+			$res = $db->query('SELECT dataJson, concernedPeople FROM Action_sous_confirmation WHERE id='.intval($idToConfirm).' AND FIND_IN_SET("'.$id.'", concernedPeople)')->fetch_array();
+
+			$ids = self::countAndOrder($res['concernedPeople']);
+
+			//Si tout le monde a confirmé (aucun refus et aucun indécis, donc)
+			if(count($ids['list?'])==0 && count($ids['list-'])==0){
+				$db->query('DELETE FROM Action_sous_confirmation WHERE id='.intval($idToConfirm)) or die(mysqli_error($db));
+				self::exec(json_decode($row['dataJson'], true));
+			}else
+				$db->query('UPDATE Action_sous_confirmation SET concernedPeople="'.$db->real_escape_string($concernedPeople).'" WHERE id='.intval($idToConfirm)) or die(mysqli_error($db));
+			return true;
+		}
+	}
+	
+}
 
 // echo 'Result : '.(confirmOrDeclineLink(1, false)?'True':'False');
 
