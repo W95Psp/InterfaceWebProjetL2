f7382521609a4262b7126076413f93dd6d1ab85a
Guillaume Seraglini <admin@Guillaumes-MacBook-Pro.local>
2015-04-10 23:00:47 +0200

    Fix pour documents PDF
    Le lien de téléchargement s’affiche pas si le projet n’a pas de
    document.

--- Interface/Sources/pages/details-project.php
+++ Interface/Sources/pages/details-project.php
@@ -15,7 +15,12 @@
 	echo "<h2>Langages du projet</h2>";
 	echo $donnee['allowedLanguages'];
 	echo "<h2>Document du projet</h2>";
-	echo "<p><a href='" . $donnee['lien'] . "' target='_blank'>Télécharger le sujet</a> (document PDF)</a>";
+	if(file_exists("pdfs/".$idProject.".pdf")){
+		echo "<p><a href='pdfs/".$idProject.".pdf' target='_blank'>Télécharger le sujet</a> (document PDF)</a>";
+	}
+	else{
+		echo "<p>Aucun document disponible pour ce projet.</p>";
+	}
 	echo "<h2>Responsable du projet</h2>";
 	$sql = "SELECT Enseignant.idEns AS idEns, Enseignant.prenomEns AS prenomEns, Enseignant.nomEns AS nomEns, Enseignant.emailEns AS emailEns FROM Enseignant,Responsable WHERE Responsable.idEns=Enseignant.idEns AND Responsable.idPro=" . $idProject;
 	$res = $db->query($sql);
