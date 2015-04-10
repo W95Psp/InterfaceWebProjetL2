ad0bd5e6a2d135abf910034c9572855a5b7b3fc6
Guillaume Seraglini <admin@Guillaumes-MacBook-Pro.local>
2015-04-10 22:55:46 +0200

    Ajout et MAJ des projets
    Page de visualisation et page de communication avec la DB.

---
+++ Interface/Sources/pages/edit-project-db.php
@@ -0,0 +1,69 @@
+
+<style>
+	.details-project img{
+		width: 20px;
+	}
+</style>
+<div ng-init="project = 0" class="page edit-project">
+	<?php
+	include "php_functions/mail-project.php";
+	$retour = "<p><a href='/?liste-des-projets'>Retour à la liste des projets</a></p>";
+	if (getUserType() >= ENCADRANT) {
+		if ((!isset($_POST['nomProj'])) || (trim($_POST['nomProj'])=="")) {
+			$nomProj = "";
+			$sortie = "Une erreur s'est produite lors de l'édition du projet.";
+			echo "<h1>Erreur</h1>";
+			echo "<p>".$sortie."</p>".$retour;
+			exit();
+		} else {
+			$nomProj = trim($_POST['nomProj']);
+			$nomProj = addslashes($nomProj);
+		}
+		if (!isset($_POST['estValide'])){
+			$estValide = 0;
+		}
+		else{
+			$estValide = 1;
+		}
+		$idProject = trim($_POST['idProject']);
+		$descProj = trim($_POST['descProj']);
+		$descProj = addslashes($descProj);
+		$allowedLanguages = trim($_POST['allowedLanguages']);
+		$allowedLanguages = addslashes($allowedLanguages);
+		if ($idProject == "N"){
+			$sql = "INSERT INTO Projet (nomProj,descProj,allowedLanguages,lien,nbMini,nbMax,estValide) VALUES ('".$nomProj."','".$descProj."','".$allowedLanguages."','',2,4,'".$estValide."')";
+			$db->query($sql);
+			$idProject = $db->insert_id;
+			$sql2 = "INSERT INTO Responsable (idPro,idEns) VALUES (".$idProject.",".getUserId().")";
+			$db->query($sql2);
+			mailValidation($idProject, $db);
+			$sortie="Le sujet a été proposé à l'administrateur.";
+		}
+		else{
+			$sql = "UPDATE Projet SET nomProj='".$nomProj."',descProj='".$descProj."',allowedLanguages='".$allowedLanguages."',estValide=".$estValide." WHERE idProj=".$idProject;
+			$db->query($sql);
+			$sortie="La modification a bien été enregistrée.";
+		}
+		if(!file_exists("pdfs")){
+			mkdir("pdfs");	
+		}
+		$pdfvalid = 1;
+		$pdf = basename($_FILES['uploadedfile']['name']);
+		$tabext = explode('.', $pdf);
+		$tabexttaille = sizeof($tabext);
+		if ($tabext[$tabexttaille-1] != "pdf") {
+			$pdfvalid = 0;
+		}
+		if ((basename($_FILES['uploadedfile']['name']) != "") && ($pdfvalid == 1)) {
+			$target_path = "pdfs/" . $idProject . ".pdf";
+			move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path);
+		}
+		
+		echo "<h1>".$nomProj."</h1>";
+		echo "<p>".$sortie."</p>".$retour;
+	} else {
+		echo "<h1>Désolé</h1>";
+		echo "<p>Vous ne disposez pas des permissions pour accéder à cette page.</p>".$retour;	
+	}
+	?>
+</div>
---
+++ Interface/Sources/pages/edit-project.php
@@ -0,0 +1,83 @@
+
+<style>
+	.details-project img{
+		width: 20px;
+	}
+</style>
+<div ng-init="project = 0" class="page edit-project">
+	<?php
+	$retour = "<p><a href='/?liste-des-projets'>Retour à la liste des projets</a></p>";
+	if (getUserType() >= ENCADRANT) {
+	?>
+		<script type="text/javascript">
+		function Valider(){
+			if (document.getElementById("nomProj").value != ""){
+				form1.submit();
+			}
+		}	
+		function Annuler(){
+			window.history.back();
+		}
+		</script>
+		<?php
+		if ((!isset($idProject)) || (trim($idProject)==0)) {
+			$idProject = "N";
+			$descProj = "";
+			$nomProj = "";
+			$allowedLanguages = "";
+			$estValide = 0;
+		} else {
+			$sql = "SELECT * FROM Projet WHERE idProj=" . $idProject;
+			$result = $db->query($sql);
+			$donnee = $result->fetch_array();
+			$idProject = $donnee['idProj'];
+			$descProj = $donnee['descProj'];
+			$nomProj = $donnee['nomProj'];
+			$allowedLanguages = $donnee['allowedLanguages'];
+			$estValide = $donnee['estValide'];
+		}
+		if ($idProject == "N") {
+			echo '<h1> Ajout d\'un projet </h1>';
+		}
+		else {
+			echo '<h1> Modification d\'un projet </h1>';
+		}
+		?>
+		
+		<form name="form1" action="edit-project-db.php" method="post" enctype="multipart/form-data">
+			<input type="hidden" id="idProject" name="idProject" value="<?php echo $idProject; ?>">
+			<h2> Nom du projet </h2>
+			<input type="text" id="nomProj" name="nomProj" value="<?php echo $nomProj; ?>" size="50">
+			<h2> Description du projet </h2>
+			<textarea id="descProj" name="descProj" rows="15" cols="70"> <?php echo $descProj; ?> </textarea>
+			<h2> Langages du projet </h2>
+			<input type="text" id="allowedLanguages" name="allowedLanguages" value="<?php echo $allowedLanguages; ?>">
+			<h2> Document du projet </h2>
+			<input type="hidden" name="MAX_FILE_SIZE" value="10000000">
+			<input name="uploadedfile" id="uploadedfile" type="file">
+			<?php
+			if (getUserType() == ADMIN) {
+				echo "<h2> Visibilité du projet </h2>";
+				if ($estValide == 0) {
+					 echo '<input id="estValide" name="estValide" value="'.$estValide.'"
+					 type="checkbox"> Mettre le projet disponible';
+				}
+				else {
+					 echo '<input id="estValide" name="estValide" value="'.$estValide.'"
+					 type="checkbox" checked> Mettre le projet disponible';
+				 }
+			 } else {
+				 echo '<input id="estValide" name="estValide" value="'.$estValide.'"
+				 type="hidden">';
+			 }
+			?>
+			<p> <input id="btnValide" name="btnValide" value="Enregistrer" type="button" onclick="Valider()">
+			<input id="annuler" name="annuler" value="Annuler" type="button" onclick="Annuler()"> </p>
+		</form>
+	<?php
+	} else {
+		echo "<h1>Désolé</h1>";
+		echo "<p>Vous ne disposez pas des permissions pour accéder à cette page.</p>".$retour;	
+	}
+	?>
+</div>
