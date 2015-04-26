187bc1b45c971d6160679ceddf7496df154ebb68
Lucas Franceschino <lucas@franceschino.fr>
2015-04-26 19:28:48 +0200

    Hide credentials

--- Interface/Sources/php_functions/mysql.php
+++ Interface/Sources/php_functions/mysql.php
@@ -1,4 +1,4 @@
 <?php
-	$db = new MySQLi('websimp.fr','root','ProjetsL2_566z9fbg','Projet') or die(mysqli_error($db));
+	$db = new MySQLi($bdHost, $bdPwd, $bdUser, $bdName) or die(mysqli_error($db));
 	$db->query("SET NAMES 'utf8'");
 ?>
